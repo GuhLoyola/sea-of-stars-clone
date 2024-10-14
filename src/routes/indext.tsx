443b53880ed4165917a19cbf133e912bd097d77f
Gustavo loyola <guh.loyola@gmail.com>
2024-10-14 11:42:30 -0300

    Rota da dlc adicionada

--- src/routes/indext.tsx
+++ src/routes/indext.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
+import DLC from "../pages/Dlc"
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path="/throes-of-the-watchmaker-dlc" element={<DLC />} />
             </Routes>
         </BrowserRouter>
     )
