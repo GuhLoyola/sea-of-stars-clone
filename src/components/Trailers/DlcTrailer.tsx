b52d9bfcdf490e401a18f91bb8230deac80b7957
Gustavo loyola <guh.loyola@gmail.com>
2024-10-14 11:43:18 -0300

    Seção de trailer da página 'DLC criada'

---
+++ src/components/Trailers/DlcTrailer.tsx
@@ -0,0 +1,11 @@
+const DlcTrailer = () => {
+    return (
+        <section className="bg-dark-midnight-blue min-h-screen flex justify-center items-center">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/yBnJv-rQjMM?si=BSeBLulG_xtDEyXo" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen
+                className="border-medium-aquamarine border-2 rounded-md w-[300px] md:w-[600px]
+                h-[180px] md:h-[420px]"></iframe>
+        </section>
+    )
+}
+
+export default DlcTrailer
