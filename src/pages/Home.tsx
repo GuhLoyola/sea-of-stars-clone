ac0f7af237929c502426ed5541004b751866aa1f
Gustavo loyola <guh.loyola@gmail.com>
2024-10-14 11:42:14 -0300

    importações alteradas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 import Footer from '../components/Footer'
-import Hero from '../components/Hero/Hero'
+import Hero from '../components/Heroes/Hero'
 import Navbar from '../components/Navbar'
 import Order from '../components/Order/Order'
-import Trailer from '../components/Trailer/Trailer'
+import Trailer from '../components/Trailers/Trailer'
 import Characters from '../components/Characters/Characters'
 
 const Home = () => {
