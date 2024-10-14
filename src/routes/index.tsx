e87c0431cedb59de7321c837eecdc2db77633d96
Gustavo loyola <guh.loyola@gmail.com>
2024-10-14 15:56:05 -0300

    Animação de transição das rotas adicionada

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,49 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom"
 import Home from "../pages/Home"
 import DLC from "../pages/Dlc"
+import { AnimatePresence, motion } from "framer-motion"
+
+const AnimatedRoutes = () => {
+
+    const location = useLocation()
+
+    return (
+        <AnimatePresence mode="wait" initial={false}>
+            <Routes location={location} key={location.pathname}>
+                <Route path='/'
+                    element={
+                        <motion.span
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0, transition: { duration: 0.5 } }}
+                        >
+                            <Home />
+                        </motion.span>
+                    }
+                />
+                <Route path="/throes-of-the-watchmaker-dlc"
+                    element={
+                        <motion.span
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                        >
+                            <DLC />
+                        </motion.span>
+                    }
+                />
+            </Routes>
+        </AnimatePresence>
+    )
+}
 
 const AppRoutes = () => {
+
+
+
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path="/throes-of-the-watchmaker-dlc" element={<DLC />} />
-            </Routes>
+            <AnimatedRoutes />
         </BrowserRouter>
     )
 }
