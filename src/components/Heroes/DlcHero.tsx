3fd1ea54adc13eb3661d82dc73412c685a53adc0
Gustavo loyola <guh.loyola@gmail.com>
2024-10-14 11:42:51 -0300

    Hero da página 'DLC' criado

---
+++ src/components/Heroes/DlcHero.tsx
@@ -0,0 +1,8 @@
+const DlcHero = () => {
+    return (
+        <section className="md:bg-dlc-hero-pattern bg-dlc-hero-pattern-mobile bg-cover bg-center min-h-screen flex justify-center items-end" />
+
+    )
+}
+
+export default DlcHero
