27bab218be91f8f9a6dd5365da1c0cae76a8408d
Gustavo loyola <guh.loyola@gmail.com>
2024-10-15 16:08:11 -0300

    Página FAQ criada

---
+++ src/pages/Faq.tsx
@@ -0,0 +1,18 @@
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+const FAQ = () => {
+    return (
+        <>
+            <header>
+                <Navbar />
+            </header>
+            <main>
+
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+export default FAQ
