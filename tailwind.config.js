6df7ade9cccb547cf496b06122e6da450f9cd4dc
Gustavo loyola <guh.loyola@gmail.com>
2024-10-17 08:58:05 -0300

    Importações de imagens alteradas

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,9 +22,9 @@ export default {
         'dark-teal': '#122A2C'
       },
       backgroundImage: {
-        'hero-pattern': "url('./src/assets/images/seaOfStars-bg.png')",
-        'dlc-hero-pattern': "url('./src/assets/images/seaOfStars-dlc-bg.png')",
-        'dlc-hero-pattern-mobile': "url('./src/assets/images/seaOfStars-dlc-bg-mobile.png')"
+        'hero-pattern': "url('src/assets/images/seaOfStars-bg.png')",
+        'dlc-hero-pattern': "url('src/assets/images/seaOfStars-dlc-bg.png')",
+        'dlc-hero-pattern-mobile': "url('src/assets/images/seaOfStars-dlc-bg-mobile.png')"
       },
       fontFamily: {
         'orpheus-pro': 'Orpheus Pro',
